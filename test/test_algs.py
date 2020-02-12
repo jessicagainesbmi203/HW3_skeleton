d73aa0051e7ff3def989f947aa2268587cde465c
Jessica Gaines <jessica.gaines@ucsf.edu>
2020-02-11 20:55:15 -0800

    Travis

--- test/test_algs.py
+++ test/test_algs.py
@@ -3,9 +3,12 @@ from smith_waterman import algs
 
 def test_roc():
     assert 1 == 1
+    return None
 
 def test_smithwaterman():
     assert 1 == 1
+    return None
 
 def test_scoring():
     assert 1 == 1
+    return None
